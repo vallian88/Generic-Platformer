e6f3a5981c5c5f048dcf0d46e4e4d3d4f76794b0
Colin R Casto <Colin@localhost.localdomain>
2018-04-28 12:56:22 -0400

    really fixed dependencies

--- Scripts/StartMenu.gd
+++ Scripts/StartMenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_StartGameButton_pressed():
-	get_tree().change_scene("res://World.tscn")
+	get_tree().change_scene("res://Scenes/World.tscn")
 
 
 func _on_QuitGameButton_pressed():
