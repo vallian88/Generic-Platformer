9addd83daab573e822d7a3e59202621a74596fc6
Michael Lee <vallian88@gmail.com>
2018-05-04 16:55:12 -0400

    Some changes

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -32,7 +32,9 @@ func _physics_process(delta):
 	else:
 		friction = true
 		$Sprite.play("Idle")
-	
+
+	if Input.is_action_pressed("ui_down") and is_on_floor():
+		print("I'm colliding!")
 
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
---
+++ Scripts/MovingPlatform.gd
@@ -0,0 +1,5 @@
+extends Sprite
+
+func _physics_process(delta):
+	position += 10
+	pass
