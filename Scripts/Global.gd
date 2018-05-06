b621a132d88a2caccc40c559acdeac361824f492
spookymakesgames <colinrcasto@gmail.com>
2018-05-06 18:03:52 -0400

    Set up physics templates

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -10,9 +10,62 @@ var props = {
         'current' : 0,
         'VULN' : 0,
         'INVULN': 1,
-    }
+    },
+	'special_state' : {
+		'current' : 0,
+		'NORMAL' : 0,
+		'ZIPPING' : 1,
+	},
+	'physics' :{
+		'GRAVITY' : 0,
+		'ACCEL' : 0,
+ 		'MAX_SPEED' : 0,
+		'JUMP_HEIGHT' : 0,
+		'GROUNDED_FRICTION' : 0,
+		'AERIAL_FRICTION' : 0,
+		'COLLIDING' : false,
+		'SCREEN_WRAP' : false
+	}
 }
 
-func Change_prop(key,val):
-    props[key]['current'] = val
-    pass
+const PROP_TEMPLATES = {
+	'NORMAL' : {
+		'GRAVITY' : 15,
+		'ACCEL' : 50,
+ 		'MAX_SPEED' : 400,
+		'JUMP_HEIGHT' : -400,
+		'GROUNDED_FRICTION' : 0.2,
+		'AERIAL_FRICTION' : 0.175,
+		'COLLIDING' : true,
+		'SCREEN_WRAP' : false
+	},
+	'ZIPPING' : {
+		'GRAVITY' : 0,
+		'ACCEL' : 300,
+ 		'MAX_SPEED' : 400,
+		'JUMP_HEIGHT' : -400,
+		'GROUNDED_FRICTION' : 0,
+		'AERIAL_FRICTION' : 0,
+		'COLLIDING' : false,
+		'SCREEN_WRAP' : true
+	},
+	'NO_CLIP' : {
+		'GRAVITY' : 15,
+		'ACCEL' : 50,
+ 		'MAX_SPEED' : 400,
+		'JUMP_HEIGHT' : -400,
+		'GROUNDED_FRICTION' : 0.2,
+		'AERIAL_FRICTION' : 0.175,
+		'COLLIDING' : false,
+		'SCREEN_WRAP' : false
+	},
+}
+
+func Change_Prop(key,val):
+	props[key]['current'] = val
+	if props.special_state.current == 0:
+		props.physics = PROP_TEMPLATES.NORMAL
+	elif props.special_state.current == 1:
+		props.physics = PROP_TEMPLATES.ZIPPING
+	
+	pass
--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -1,37 +1,51 @@
 extends KinematicBody2D
 
+
+#const GRAVITY = 15
+#const ACCEL = 50
+#const MAX_SPEED = 400
+#const JUMP_HEIGHT = -400
+#const GROUNDED_FRICTION = 0.2
+#const AERIAL_FRICTION = 0.175
 const UP = Vector2(0,-1)
-const GRAVITY = 15
-const ACCEL = 50
-const MAX_SPEED = 400
-const JUMP_HEIGHT = -400
-const GROUNDED_FRICTION = 0.2
-const AERIAL_FRICTION = 0.175
-
+onready var g = get_node('/root/Global')
 var motion = Vector2()
-var current_friction = GROUNDED_FRICTION
+var current_friction = 0
 
 
-
+func _ready():
+	g.Change_Prop('special_state', 0)
+	current_friction = g.props.physics.GROUNDED_FRICTION
+	
+	pass
 
 func _physics_process(delta):
 	
 	if is_on_floor():
-		current_friction = GROUNDED_FRICTION
+		current_friction = g.props.physics.GROUNDED_FRICTION
 	else:
-		current_friction = AERIAL_FRICTION
+		current_friction = g.props.physics.AERIAL_FRICTION
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed('ui_up'):
-			motion.y = JUMP_HEIGHT
+			motion.y = g.props.physics.JUMP_HEIGHT
 	
 	if Input.is_action_pressed('ui_right'):
-		motion.x = min(motion.x+ACCEL, MAX_SPEED)
+		motion.x = min(motion.x+g.props.physics.ACCEL, g.props.physics.MAX_SPEED)
 	
 	if Input.is_action_pressed('ui_left'):
-        motion.x = max(motion.x-ACCEL,-MAX_SPEED)
+        motion.x = max(motion.x-g.props.physics.ACCEL,-g.props.physics.MAX_SPEED)
+		
+	if Input.is_action_pressed('ui-down'):
+		pass
     
-	motion.y += GRAVITY
+	motion.y += g.props.physics.GRAVITY
 	motion.x = lerp(motion.x,0,current_friction)
 	motion = move_and_slide(motion,UP)
 	pass
+
+func _on_Area2D_body_entered(body):
+	if body.get_collision_layer_bit(1) == true:
+		g.Change_Prop('special_state', 1)
+		pass
+	pass 
